adf293f877c94340bf15bd45508f0fa523a06ba3
Rafael Gaspar <rafael.gaspar97@gmail.com>
2020-02-08 02:49:47 +0000

    finished header and added homepage image

---
+++ wp-content/themes/Habispire/front-page.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<div class="home-image-container">
+    <img src="<?php echo get_bloginfo('template_url') ?>/assets/images/homepage_image_cropped.jpg" alt="Homepage image">
+    sup
+</div>
+
+<?php get_footer(); ?>
